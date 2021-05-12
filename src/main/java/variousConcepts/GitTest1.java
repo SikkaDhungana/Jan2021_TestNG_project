d52a302b620c5569dc103b7dc7916106900ea7ae
SikkaDhungana <sikkadhungana@gmail.com>
2021-05-12 16:30:43 -0400

    3rd commit

---
+++ src/main/java/variousConcepts/GitTest1.java
@@ -0,0 +1,22 @@
+package variousConcepts;
+
+public class GitTest1 {
+
+
+	public static void main(String[] args) {
+	 printMe();
+	 printMee();
+	}
+	
+	public static void printMe() {
+		System.out.println("git test");
+		System.out.println("git test111");
+	}
+	
+	public static void printMee() {
+		System.out.println("git test");
+		System.out.println("git test1");
+	}
+	
+	
+}
