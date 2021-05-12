b7ea79aec9355434564ce4130f620f6e71b9142a
SikkaDhungana <sikkadhungana@gmail.com>
2021-05-12 16:44:32 -0400

    4th commit

--- src/main/java/variousConcepts/GitTest1.java
+++ src/main/java/variousConcepts/GitTest1.java
@@ -4,19 +4,28 @@ public class GitTest1 {
 
 
 	public static void main(String[] args) {
-	 printMe();
-	 printMee();
+	 printMe1();
+	 printMee2();
+	 printMee3();
+	 printMee4();
 	}
 	
-	public static void printMe() {
+	public static void printMe1() {
 		System.out.println("git test");
 		System.out.println("git test111");
 	}
 	
-	public static void printMee() {
+	public static void printMee2() {
 		System.out.println("git test");
 		System.out.println("git test1");
 	}
 	
-	
+	public static void printMee3() {
+		System.out.println("git test");
+		System.out.println("git test1");
+	}
+	public static void printMee4() {
+		System.out.println("git test");
+		System.out.println("git test1");
+	}
 }
